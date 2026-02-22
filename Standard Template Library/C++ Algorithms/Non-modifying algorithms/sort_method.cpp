ab7f5e3e38dd0da65d34ca97574e90e1b201b34a
devisingh rajput <devisinghrajput.cg@gmail.com>
2026-02-22 16:36:38 +0530

    Add sort_method.cpp

---
+++ Standard Template Library/C++ Algorithms/Non-modifying algorithms/sort_method.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+// test in
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> num = {1, 2, 3, 6, 5, 87, 26, 8, 2, 399, 75, 3, 8, 2, 5, 87, 42, 6, 99, 7, 1};
+
+   sort(num.begin(),num.end());
+
+   for (auto & i : num)
+   {
+    cout<<i<<" ";
+   }
+   
+
+    return 0;
+}
